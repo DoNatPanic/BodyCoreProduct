1a1f21d071f174acd91ed4743eba59ce1b735c8c
Faina <fainashigapova.post@gmail.com>
2020-06-13 14:54:48 +0300

    New sitemap structure, delete robots file

--- Startup.cs
+++ Startup.cs
@@ -20,8 +20,6 @@ namespace BodyCore
 			services.AddMvc(option => option.EnableEndpointRouting = false);
 		}
 
-	
-
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure( IApplicationBuilder app, IWebHostEnvironment env )
 		{
